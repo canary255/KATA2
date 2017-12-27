93ecd88b5ac26dfb1b56652cc0af81ca2528e4a8
ismael <ismael@192.168.1.34>
2017-12-27 15:38:00 +0000

    KATA2 ver 2.0

--- src/kata2/KATA2.java
+++ src/kata2/KATA2.java
@@ -3,20 +3,17 @@ package kata2;
 import java.util.*;
 
 public class KATA2{
-   public static void main(String[] args) {
-       int [] data = {1,1,1,0,1,2,1,0,3};
-       Map<Integer, Integer> myMap = new HashMap<>();
+    public static void main(String[] args) {
+        int [] data = {1,1,1,0,1,2,1,0,3};
+        Map<Integer, Integer> myMap = new HashMap<>();
        
-       for (int i = 0; i < data.length; i++) {
-           if(myMap.containsKey(data[i])){
-               myMap.put(data[i], myMap.get(data[i]) + 1);
-           } else {
-               myMap.put(data[i], 1);
-           }
-       }
+        for (int key : data){
+            myMap.put(key, myMap.containsKey(key) ?
+            myMap.get(key) + 1 : 1);
+        }
        
-       for (int key : myMap.keySet()){
+        for (int key : myMap.keySet()){
            System.out.println(key + " ==> " + myMap.get(key) + " veces");
-       }
-   }  
+        }
+    }  
 }
